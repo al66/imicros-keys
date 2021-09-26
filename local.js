3d21e4be5931424ff4b34dcadea61498f5e1270c
al66 <andreas.leinen@imicros.de>
2021-09-26 16:56:38 +0200

    added cassandra port and credential settings

--- local.js
+++ local.js
@@ -1,12 +1,10 @@
 process.env.CASSANDRA_CONTACTPOINTS = "192.168.2.124";
 process.env.CASSANDRA_DATACENTER = "datacenter1";
 process.env.CASSANDRA_KEYSPACE = "imicros_keys";
+process.env.CASSANDRA_PORT = 31326;
+process.env.CASSANDRA_USER = "cassandra";
+process.env.CASSANDRA_PASSWORD = "cassandra";
 
-process.env.REDIS_HOST = "192.168.2.124";
-process.env.REDIS_PORT = 6379;
-process.env.REDIS_AUTH = "";
-process.env.REDIS_DB = 1;
-process.env.NATS_URL = "nats://192.168.2.124:4222";
 module.exports = {
     "collectCoverageFrom": [
         "lib/*.js",
